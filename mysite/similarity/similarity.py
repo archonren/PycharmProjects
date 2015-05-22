62038c06042870fe7eabdd91c62adab219c971a0
archonren <archon_ren@hotmail.com>
2015-05-22 17:21:35 -0400

    update similarity

---
+++ mysite/similarity/similarity.py
@@ -0,0 +1,102 @@
+from tag_bag import *
+from scivq import *
+from user_data import *
+from gensim.models import Word2Vec
+from gensim import matutils
+from numpy import float32 as REAL,array,sum,zeros
+from scipy.stats.stats import pearsonr
+
+class data(object):
+    def __init__(self,tag_data,user_data,k,path):
+        self.tag_data = tag_data
+        self.user_data = user_data
+        self.k = k
+        self.model = Word2Vec.load_word2vec_format(path, binary=True)
+        self.minimium_model = {}
+        self.no_match_tag = []
+        self.vec_dict = {}
+        self.corr_dict = {}
+    def get_minimium_model(self):
+        '''
+        :return: minimium model: dict contain just the tag in the bank and corresponding 300 dim vector
+        '''
+        for item in self.tag_data:
+            try:
+                vec = self.model[item]
+                self.minimium_model[item] = vec
+            except KeyError:
+                try:
+                    mean = []
+                    for word in item.split():
+                        mean.append(self.model[word])
+                    mean = matutils.unitvec(array(mean).mean(axis=0)).astype(REAL)
+                    self.minimium_model[item] = mean
+                except KeyError:
+                    print('ooops!',item,"does not appear in the google database")
+                    self.no_match_tag.append(item)
+
+
+
+    def clustering(self):
+        '''
+        :return: dict that tells which tag belongs to which cluster {key: tag, value: cluster id}
+        '''
+        vec = []
+        vec_key = []
+        for key in self.minimium_model.keys():
+            vec_key.append(key)
+            vec.append(self.minimium_model[key])
+        vec_array = array(vec).reshape((-1,300))
+        centers,dist = kmeans(vec_array,self.k)
+        code,distance = vq(vec_array,centers)
+        for i in range(len(code)):
+            self.vec_dict[vec_key[i]] = code[i]
+
+
+    def vote(self,topn = 3):
+        '''
+        :param vec_dict: dict that tells which tag belongs to which cluster {key: tag, value: cluster id}
+        :param user_data: user data
+        :param k: number of cluster
+        :param topn: optional, default 3, return top n similar user
+        :return: dict that contain top n most similar user {key : user, value: suggest users}
+        '''
+        user_item_dict = {}
+        for key in self.user_data.keys():
+            vote_array = zeros((self.k,1))
+            for item in self.user_data[key]:
+                vote_array[self.vec_dict[item]] += 1
+            user_item_dict[key] = vote_array/sum(vote_array)
+
+        for key1 in self.user_data.keys():
+            corr_max = -1
+            dict = {}
+            short_list = []
+            for key2 in self.user_data.keys():
+                if key2!=key1:
+                    dict[key2] = (pearsonr(user_item_dict[key1],user_item_dict[key2])[0])
+            most_similar = sorted(dict, key=dict.get,reverse=True)
+            for i in range(topn):
+                short_list.append(most_similar[i])
+            self.corr_dict[key1] = short_list
+
+
+
+def similarity():
+    '''
+    :param tag_data: tag data (array)
+    :param user_data: user data (dict)
+    :param k: number of cluster
+    :return: dict that tells which tag belongs to which cluster {key: tag, value: cluster id}
+'''
+
+    #sample running code
+    k=8
+    tag_data = tag_bank()
+    user_data,data_in = loadData(tag_data)
+    x = data(tag_data,user_data,k,'c:/GoogleNews.bin')
+    x.get_minimium_model()
+    x.clustering()
+    x.vote()
+    print(x.corr_dict)
+similarity()

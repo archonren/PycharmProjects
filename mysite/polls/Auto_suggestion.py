1b4e60c21bcc1061b7e69fc6c807c6a0d0897ac8
archonren <archon_ren@hotmail.com>
2015-05-22 22:13:56 -0400

    minor change suggestion

---
+++ mysite/polls/Auto_suggestion.py
@@ -0,0 +1,43 @@
+from FP_tree import *
+import data_in
+class auto_suggest(object):
+    def __init__(self,user_data):
+        self.user_data = user_data
+
+        self.F = []
+        self.support_data= {}
+
+        self.H = []
+        self.rules = {}
+
+        self.response_data_user = {}
+        self.response_data_tag = {}
+
+    def find_rules(self):
+        self.F,self.support_data = fpgrowth(self.user_data.values(), 0.6)
+        self.H,self.rules = generate_rules(self.F, self.support_data, 0.6)
+
+    def suggestion_for_user(self):
+        for user in self.user_data.keys():
+            suggestion = set([])
+            for rule in self.rules:
+                if self.rules[rule]['prior'].issubset(self.user_data[user]):
+                    suggestion = suggestion.union(self.rules[rule]['post'])
+            self.response_data_user[user] = list(suggestion)
+
+    def suggestion_for_tag(self,tag):
+        key = frozenset(tag)
+        if key in self.rules.keys():
+            return self.rules[key]['post']
+        else:
+            return {}
+
+
+def post():
+
+    data,in_data = data_in.loadData(1)
+    a = auto_suggest(data)
+    a.find_rules()
+    a.suggestion_for_user()
+    print(a.response_data_user)
+    #tag_suggestion = suggestion_for_tag(rules,tag)

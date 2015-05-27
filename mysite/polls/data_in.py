1832857b4c7b1627d8a0e5780f865fe794ccab36
Stian Håklev <shaklev@gmail.com>
2015-05-27 19:47:09 +0800

    Mefi tags - not working

--- mysite/polls/data_in.py
+++ mysite/polls/data_in.py
@@ -1,13 +1,15 @@
+import json
 def loadData():
-    data = \
-        {
-            'User A':['art', 'phy', 'history', 'career', 'esl'],
-            'User B':['phy', 'chem', 'math', 'writing', 'visual', 'university', 'health', 'social'],
-            'User C':['phy'],
-            'User D':['art', 'math', 'number', 'bio', 'social'],
-            'User E':['chem', 'art', 'math', 'phy', 'native', 'health', 'esl'],
-            'User F':['chem', 'phy', 'math', 'tech', 'native', 'social', 'health', 'music'],
-            'User G':['chem', 'math', 'tech', 'social', 'health', 'music'],
-            'User H':['chem', 'math', 'tech', 'tech', 'health', 'music']
-        }
+    # data = \
+    #     {
+    #         'User A':['art', 'phy', 'history', 'career', 'esl'],
+    #         'User B':['phy', 'chem', 'math', 'writing', 'visual', 'university', 'health', 'social'],
+    #         'User C':['phy'],
+    #         'User D':['art', 'math', 'number', 'bio', 'social'],
+    #         'User E':['chem', 'art', 'math', 'phy', 'native', 'health', 'esl'],
+    #         'User F':['chem', 'phy', 'math', 'tech', 'native', 'social', 'health', 'music'],
+    #         'User G':['chem', 'math', 'tech', 'social', 'health', 'music'],
+    #         'User H':['chem', 'math', 'tech', 'tech', 'health', 'music']
+    #     }
+    data=json.load(open("mefi.json"))
     return data
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,21 @@
-import data_in,suggestion
 import sys
 import json
 
-def post():
-    data = data_in.loadData()
+import data_in    # mock data, currently MetaFilter data dump
+import suggestion # the code making the suggestion
+
+
+def post(data):
     a = suggestion.auto_suggest(data)
     a.find_rules()
     a.suggestion_for_user()
-    return json.dumps(a.response_data_user)
+    return a.response_data_user
+
+def compare(input):
+    data = data_in.loadData()
+    data["USER"] = input.split(",")
+    proc = post(data)
+    print(proc["USER"])    
 
 if __name__ == '__main__':
-    print(post())
-
+    compare(sys.argv[1])

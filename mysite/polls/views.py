924e2897a9854db052518893ca7bd0432b90b25f
archonren <archon_ren@hotmail.com>
2015-05-26 20:41:35 -0400

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,20 +1,25 @@
 from django.http import HttpResponse
 import json,urllib.request
 import data_in,suggestion
-import argparse
-def post():
+from simplejson.compat import StringIO
+import urllib.request
+import data_in,suggestion
+import sys
+def post(arg):
     #url = "http://maps.googleapis.com/maps/api/geocode/json?address=googleplex&sensor=false"
-    parser = argparse.ArgumentParser(description='Pass a file url')
-    parser.add_argument('input_url',
-                   help='an file url')
 
-    argument = parser.parse_args()
-    req = urllib.request.Request(argument.input_url)
+    req = urllib.request.Request(arg)
     response = urllib.request.urlopen(req)
     data,in_data = data_in.loadData(response)
     a = suggestion.auto_suggest(data)
     a.find_rules()
     a.suggestion_for_user()
-    print(a.response_data_user)
+    io = StringIO()
+    json.dump(a.response_data_user, io)
+    io.getvalue()
     #tag_suggestion = suggestion_for_tag(rules,tag)
+
+
+if __name__ == '__main__':
+    post(sys.argv[1])
 
